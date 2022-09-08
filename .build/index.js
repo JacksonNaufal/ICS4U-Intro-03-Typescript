b1a1bc1f86672c3fc1559e9b4650a79d8d47b036
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-09-08 22:01:15 +0000

    Initial commit

---
+++ .build/index.js
@@ -0,0 +1,7 @@
+let message = "Hello, World!";
+console.log(message);
+let number = "10 + 4";
+console.log(number);
+let answer = 10 + 4;
+console.log(answer);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,6 @@
+let message: string = 'Hello, World!';
+console.log(message)
+let number: int = '10 + 4';
+console.log(number)
+let answer: int = 10 + 4;
+console.log(answer)
